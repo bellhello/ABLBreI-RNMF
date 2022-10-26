28f9b3aadcdd06836260176f0897e5dac6de4961
bel <bell's email>
2022-10-26 21:29:18 +0800

    10.26

--- BLBreIF.jl
+++ BLBreIF.jl
@@ -63,7 +63,12 @@ function update_xy!(upd::BLBreIFUpd{T}, s::BLBreIFUpd_State{T}, A, X:Matrix{T}, 
     v = soft_thresholding(rho*Vx, mu*rho)
     t_0 = find_zero(norm(v, 2)^2*t^3 + t - 1, (0, 10))
     x_1 = -t_0*v
-    px[:, j_k] = px[:, j_k] - 1/rho*((norm(x_1, 2)^2 + norm(Y[j_k, :],2)^2 + 1)*x - (norm(X[:, j_k], 2)^2 + norm(Y[j_k, :], 2)^2 + 1)*X[:, j_k] + rho*(XY - A)*Y[j_k, :])
+    px[:, j_k] = px[:, j_k] - 1/rho*((norm(x_1, 2)^2 + norm(Y[j_k, :],2)^2 + 1)*x_1 - (norm(X[:, j_k], 2)^2 + norm(Y[j_k, :], 2)^2 + 1)*X[:, j_k] + rho*(XY - A)*Y[j_k, :])
 
     # update y_j
     Vy[j_k, :] = 1/rho*(norm(x_1, 2)^2 + norm(Y[j_k, :]^2 + 1)*Y[j_k, :]+ mu*py[j_k, :] - (XY - X[:, j_k]*transpose(Y[j_k, :]) + x_1*transpose(Y[j_k, :]) - A)*x_1)
+    v = soft_thresholding(rho*Vy, mu*rho)
+    t_0 = find_zero(norm(v, 2)^2*t^3 + t -1, (0, 10))
+    y_1 = -t_0*v
+    py[j_k, :] = py[j_k, :] - 1/rho*((norm(x_1, 2)^2 + norm(y_1, 2)^2 + 1)*y_1 - (norm(x_1, 2)^2 + norm(Y[j_k, :], 2)^2 + 1)*Y[j_k, :] + rho*transpose(XY - X[:, j_k]*transpose(Y[j_k, :]) + x_1*transpose(Y[j_k, :]) - A)*x_1)
+
