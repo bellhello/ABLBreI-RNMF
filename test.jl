5ffec0076b8b0603bee82cab7e14be222ce1aa4b
dell <cbe20000704@126.com>
2022-11-15 13:06:45 +0800

    11.15

--- test.jl
+++ test.jl
@@ -2,8 +2,9 @@ include("NMF.jl")
 A = rand(Float64, 200, 200)
 A = BLNMF.normalize!(A)
 X, Y = BLNMF.randinit(A, 10)
+X0 = copy(X); Y0 = copy(Y);
 r = BLNMF.solve!(BLNMF.BLBreIF{Float64}(maxiter = 1000,
     verbose = true,
     tol = 432,
-    rho = 0.5,
-    mu = 0.001), A, X, Y)
+    rho = 0.2,
+    mu = 0.001), A, X0, Y0)
